617bb219423d126306db5e79a53f2df869b9d1b8
Заян Дандляев <zayandandlyaev@MacBook-Air-Zaan.local>
2022-02-21 11:22:22 +0300

    test6

--- api_yamdb/api/serializers/token.py
+++ api_yamdb/api/serializers/token.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework_simplejwt.tokens import AccessToken
 from users.models import User
 
 
@@ -10,8 +9,3 @@ class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'confirmation_code']
-    
-    @classmethod
-    def get_token(cls, user):
-        token = AccessToken.for_user(user)
-        return token
